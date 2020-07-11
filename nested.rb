e25c16ab9f4eac46b8535b2b6cc26cbbf9aea43c
Jonathan Chasse <jonathan.r.chasse@gmail.com>
2020-07-11 14:30:35 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -112,7 +112,7 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     programer_hash[:alan_kay][:known_for] = "GUI"
+     programmer_hash[:alan_kay][:known_for] = "GUI"
 
 return programer_hash
 
